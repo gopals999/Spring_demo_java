4e1fcf25c52ba50348a2a2d58d95b43ceaf5a616
sandy <gopals.jd@gmail.com>
2017-01-22 16:46:52 -0700

    added line

--- calc.py
+++ calc.py
@@ -2,4 +2,4 @@ You are downloading the latest (2.11.0) 64-bit version
  of Git for Windows. This is the most recent maintained build. 
  It was released 8 days ago, on 2017-01-14.
  
- maintained build. s
+ maintained build of the git version .
